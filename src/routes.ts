b418aa41ad6c45ea90ad83b06bd283674e55d721
Volnei Neves <volneinevesfilho@hotmail.com>
2022-08-02 07:29:51 -0300

    Client Authentication implemented

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
+import { AuthenticateClientController } from "./modules/account/useCases/AuthenticateClientController";
 import { CreateClientController } from "./modules/client/useCases/createClient/CreateClientController";
 
 const routes = Router();
 
 const createClientController = new CreateClientController();
+const authenticateClientController = new AuthenticateClientController();
 
-routes.post('/client/', createClientController.handle);
+routes.post("/authenticate", authenticateClientController.handle);
+routes.post("/client/", createClientController.handle);
 
 export { routes };
