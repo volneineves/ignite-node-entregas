7eb340423ff36867d54c27da51c724472d1b4a91
Volnei Neves <volneinevesfilho@hotmail.com>
2022-08-02 23:54:24 -0300

    Deliveryman authentication was implemented

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
-import { AuthenticateClientController } from "./modules/account/useCases/AuthenticateClientController";
+import { AuthenticateClientController } from "./modules/account/authenticateClient/AuthenticateClientController";
+import { AuthenticateDeliverymanController } from "./modules/account/authenticateDeliveryman/AuthenticateDeliverymanController";
 import { CreateClientController } from "./modules/client/useCases/createClient/CreateClientController";
 import { CreateDeliverymanController } from "./modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController";
 
@@ -7,9 +8,15 @@ const routes = Router();
 
 const createClientController = new CreateClientController();
 const authenticateClientController = new AuthenticateClientController();
+const authenticateDeliverymanController =
+  new AuthenticateDeliverymanController();
 const createDeliverymanController = new CreateDeliverymanController();
 
-routes.post("/authenticate", authenticateClientController.handle);
+routes.post("/client/authenticate", authenticateClientController.handle);
+routes.post(
+  "/deliveryman/authenticate",
+  authenticateDeliverymanController.handle
+);
 routes.post("/client/", createClientController.handle);
 routes.post("/deliveryman", createDeliverymanController.handle);
 
