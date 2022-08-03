3eecb9ccbe0a64b372ca45ba9846115e18085483
Volnei Neves <volneinevesfilho@hotmail.com>
2022-08-02 23:44:54 -0300

    Deliveryman register implemented

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,16 @@
 import { Router } from "express";
 import { AuthenticateClientController } from "./modules/account/useCases/AuthenticateClientController";
 import { CreateClientController } from "./modules/client/useCases/createClient/CreateClientController";
+import { CreateDeliverymanController } from "./modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController";
 
 const routes = Router();
 
 const createClientController = new CreateClientController();
 const authenticateClientController = new AuthenticateClientController();
+const createDeliverymanController = new CreateDeliverymanController();
 
 routes.post("/authenticate", authenticateClientController.handle);
 routes.post("/client/", createClientController.handle);
+routes.post("/deliveryman", createDeliverymanController.handle);
 
 export { routes };
